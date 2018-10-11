bae8c2012139fa3a32e2adfaad67a7cf7ad12eb9
lucicd <lucicd@hotmail.com>
2018-10-11 16:23:38 +0400

    Finished walkthrough step 9

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+	'sap/ui/core/mvc/Controller',
+  'sap/m/MessageToast',
+], function (Controller, MessageToast) {
+	'use strict';
 
-	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
+	return Controller.extend('mynamespace.walkthrough.controller.App', {
 		onShowHello : function () {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
-			alert("Hello World");
-			/* eslint-enable no-alert */
-		}
+      // read msg from i18n model
+      var oBundle = this.getView().getModel('i18n').getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty('/recipient/name');
+      var sMsg = oBundle.getText('helloMsg', [sRecipient]);
+      // show message
+      MessageToast.show(sMsg);
+		},
 	});
 
 });
